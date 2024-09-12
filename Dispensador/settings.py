7557bcc224922a08890d50690792316baddd9c1b
Desarrollo2-1989 <practicante.sistemas@glingenieros.co>
2024-09-12 14:09:57 -0500

    subiendo mi segundo comentario

--- Dispensador/settings.py
+++ Dispensador/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rf58ip&+p!urt_gfc7v2+7pd#mut!gt^@rg-@14e1igmio7qu*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.8.87']
 
